3d8c2da6791ffd88c22b7be6d4470f5a5c4a45fb
Muhammad-javed440 <meherjaved440@gmail.com>
2024-04-07 04:58:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    DIN: 40,
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: 'Enter from currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'DIN']
+    },
+    {
+        name: 'to',
+        message: 'Enter to currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'DIN']
+    },
+    {
+        name: 'amount',
+        message: 'Enter your amount',
+        type: 'number'
+    }
+]);
+console.log(userAnswer);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount; //  USD base currenecy
+let convertedAmount = baseAmount * toAmount;
+let convertedAmount1 = parseFloat(convertedAmount.toFixed(2));
+console.log(`   \n From Country exchange rate: ${fromAmount}\n   `);
+console.log(`   \nTo country exchange rate: ${toAmount}\n  `);
+console.log(`   \nEnter amount : ${amount}\n  `);
+console.log(`   \nConverted Amount : ${convertedAmount1}\n  `);
